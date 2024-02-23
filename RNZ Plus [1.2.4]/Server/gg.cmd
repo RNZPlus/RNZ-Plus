4130a9b760c534a0927a851e0d09eabb18ddab0b
RNZPlus <161056377+RNZPlus@users.noreply.github.com>
2024-02-23 19:15:19 +0100

    Add files via upload

---
+++ RNZ Plus [1.2.4]/Server/gg.cmd
@@ -0,0 +1,37 @@
+color a
+start "" cmd /c "echo Hello world!&echo
+color a
+tree
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+start "" cmd /c "echo Hello world!&echo
+color a
+color c
+start "" cmd /c "echo You can buy RNZ Ultimate for ONLY 4.99$ &echo(&pause"
+start "" cmd /c "echo RNZ Plus was been Activated&echo(&pause"
